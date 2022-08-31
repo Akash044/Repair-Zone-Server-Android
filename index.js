2ca84515692be81a49e1968721bcf50d9488ed38
Akash374 <akash.hossain@dataphilics.in>
2022-09-01 01:30:38 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+
 const bodyParser = require('body-parser');
+
 const cors = require('cors');
+
 const fileUpload = require('express-fileupload');
+
 const ObjectId = require('mongodb').ObjectID;
 const MongoClient = require('mongodb').MongoClient;
+
+
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 const saltRounds = 10;
@@ -12,6 +18,7 @@ require('dotenv').config();
 const app = express();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ieei5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json({ limit: '50mb' }));
@@ -38,81 +45,11 @@ client.connect(err => {
 
    const userCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL1);
    const serviceCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL2);
-   // const rentCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL3);
-   // const mealCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL4);
-   // const roomCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL5);
-   // const mealRateCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL6);
-   // const guestCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL7);
+   const orderCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COL3);
+
+   console.log("connected")
 
 
-   app.post('/addGuest', (req, res) => {
-      const guestImg = req.body.base64;
-      const imgSize = req.body.fileSize;
-      const type = req.body.type;
-      const name = req.body.guestName;
-      const mobile = req.body.mobile;
-      const address = req.body.address;
-      const date = req.body.today;
-      const relativeEmail = req.body.relativeEmail;
-
-      const image = {
-         contentType: type,
-         size: imgSize,
-         img: Buffer.from(guestImg, 'base64')
-      };
-      const guestInfo = { name: name, mobile: mobile, address: address, date: date, relativeEmail: relativeEmail, ...image }
-
-      guestCollection.insertOne(guestInfo)
-         .then(result => {
-            res.send(result.insertedCount > 0)
-         })
-   })
-
-   app.post('/addMealRate', (req, res) => {
-      const mealRateInfo = req.body;
-      mealRateCollection.insertOne(mealRateInfo)
-         .then(result => {
-            res.send(result.insertedCount > 0)
-         })
-   })
-   app.get("/allMealRate", (req, res) => {
-      mealRateCollection.find({})
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
-
-   app.get('/boarders', (req, res) => {
-      boarderCollection.find({})
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
-   app.get('/boarder/:email', (req, res) => {
-      const userEmail = req.params.email;
-      boarderCollection.find({ email: userEmail })
-         .toArray((err, documents) => {
-            res.send(documents[0]);
-         })
-   })
-
-   app.patch('/boarder/:email', (req, res) => {
-      boarderCollection.updateOne({ email: req.params.email },
-         {
-            $set: {
-               name: req.body.name,
-               dept: req.body.dept,
-               sec: req.body.sec,
-               roll: req.body.roll,
-               address: req.body.address,
-               mobile: req.body.mobile,
-               bp: req.body.bp,
-            }
-         })
-         .then((result) => {
-            res.send(result.modifiedCount > 0)
-         });
-   })
 
    app.get('/isUser', (req, res) => {
       boarderCollection.find({ email: req.query.email })
@@ -151,18 +88,20 @@ client.connect(err => {
          })
    })
    app.post('/signup', async (req, res) => {
-      const { name, contact, email, address, password ,admin} = req.body;
+      const { name, contact, email, address, password, admin, user } = req.body;
       console.log(req.body)
 
       userCollection.find({ email: email, verified: true })
-         .toArray(async (err, result) => {
+         .toArray((err, result) => {
+            console.log(result)
             if (result.length > 0) {
+               console.log("user not found")
                res.send(false);
             }
             else {
-               bcrypt.genSalt(saltRounds, async (err, salt) => {
-                  bcrypt.hash(password, salt, async (err, hashPassword) => {
-                     console.log(hashPassword)
+               bcrypt.genSalt(saltRounds, (err, salt) => {
+                  bcrypt.hash(password, salt, (err, hashPassword) => {
+                     console.log("hashPassword--->", hashPassword)
                      let transporter = nodemailer.createTransport({
                         service: 'gmail',
                         auth: {
@@ -181,13 +120,13 @@ client.connect(err => {
                            html: `<p>Enter <b>${otp}</b> in the app to verify your email address and complete registration.</p><p>This code <b>expires in 5 minutes</b>.</p>`,
                         };
 
-                        const hashOTP = await bcrypt.hash(otp, saltRounds);
+                        const hashOTP = bcrypt.hash(otp, saltRounds);
                         console.log(hashOTP);
 
 
                         transporter.sendMail(mailOptions, (err, resp) => {
                            if (err) {
-                              console.log(err);
+                              console.log("transporter error-->", err);
                               mailResponse = false
                               res.send(false)
 
@@ -215,11 +154,12 @@ client.connect(err => {
                                  verified: false,
                                  otp: hashOTP,
                                  admin: admin,
+                                 user: user,
                                  ...image
 
                               }
                               userCollection.find({ email: email, verified: false })
-                                 .toArray(async (err, result) => {
+                                 .toArray((err, result) => {
                                     console.log(result.length);
                                     if (result.length > 0) {
                                        userCollection.deleteMany({ email: email })
@@ -244,7 +184,7 @@ client.connect(err => {
                                     else {
                                        userCollection.insertOne(newUser)
                                           .then(result => {
-                                             console.log("ascheeee",result)
+                                             console.log("ascheeee", result)
                                              res.send(result.acknowledged);
                                           })
                                     }
@@ -254,6 +194,7 @@ client.connect(err => {
 
                      }
                      catch (err) {
+                        console.log(err.message);
 
                      }
                      // user.insertOne({ ...borderInfo, password: hash })
@@ -268,71 +209,7 @@ client.connect(err => {
 
 
    })
-   app.post('/uploadRoomImg', (req, res) => {
-      const boarderImg = req.body.base64;
-      const imgSize = req.body.fileSize;
-      const type = req.body.type;
-      const image = {
-         contentType: type,
-         size: imgSize,
-         img: Buffer.from(boarderImg, 'base64')
-      };
-      boarderCollection.insertOne(image)
-         .then(result => {
-            res.send(result.insertedCount > 0);
-         })
-   })
 
-   app.patch('/paidRents/:id', (req, res) => {
-
-      let message = '';
-      let mailResponse = false;
-      if (req.body.status == "pending") {
-         message = `Your payment (TrxID: ${req.body.trxID}) is being processed. Please wait for it to be processed.
-                 Thank you`
-      } else if (req.body.status == "paid") {
-         message = `Your payment (TrxID: ${req.body.trxID}) has been completed.
-                 Thank you`
-      } else {
-         message = `Your payment (TrxID: ${req.body.trxID}) is unsuccessful. Please check the number and try again. If anything done wrong from you, I don't have any way to resolve this. Otherwise contact with me asap.
-         Thank you`
-      }
-
-
-      rentCollection.updateOne({ trxId: req.params.id },
-         {
-            $set: { status: req.body.status }
-         })
-         .then((result) => {
-
-            let transporter = nodemailer.createTransport({
-               service: 'gmail',
-               auth: {
-                  user: "ruetakash@gmail.com",
-                  pass: "1703044A"
-               }
-            });
-            let mailOptions = {
-               from: "ruetakash@gmail.com",
-               to: req.body.email,
-               subject: 'Rent Status',
-               text: message
-            };
-            transporter.sendMail(mailOptions, (err, resp) => {
-               if (err) {
-                  console.log(err);
-                  mailResponse = false
-                  result.modifiedCount > 0 && res.send({ updateInfo: true, mailResponse })
-
-               }
-               else {
-                  console.log(resp);
-                  mailResponse = true;
-                  result.modifiedCount > 0 && res.send({ updateInfo: true, mailResponse })
-               }
-            });
-         })
-   })
 
    app.get('/isAdmin', (req, res) => {
       adminCollection.find({ email: req.query.email })
@@ -340,74 +217,7 @@ client.connect(err => {
             res.send(documents.length > 0);
          })
    })
-   app.post('/addAdmin', (req, res) => {
-      adminCollection.insertOne(req.body)
-         .then(result => {
-            res.send(result.insertedCount > 0)
-         })
-   })
 
-
-   app.post('/addMeal', (req, res) => {
-      mealCollection.insertOne(req.body)
-         .then(result => {
-            res.send(result.insertedCount > 0)
-         })
-   })
-
-   app.get('/boarderMeal/:email', (req, res) => {
-      mealCollection.find({ email: req.params.email })
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
-
-   app.post('/addRent', (req, res) => {
-      rentCollection.insertOne(req.body)
-         .then(result => {
-
-            if (result.insertedCount > 0) {
-
-               let mailResponse = false;
-
-
-               let mailOptions = {
-                  from: process.env.GMAIL,
-                  to: req.body.email,
-                  subject: 'Rent Status',
-                  text: `Your payment (TrxID: ${req.body.trxId}) is being processed. Please wait for it to be processed.
-               Thank you`
-               };
-               transporter.sendMail(mailOptions, (err, resp) => {
-                  if (err) {
-                     console.log(err);
-                     mailResponse = false;
-                  }
-                  else {
-                     mailResponse = true;
-                  }
-               });
-               res.send({ updateInfo: true, mailResponse })
-
-            } else {
-               res.send({ updateInfo: true, mailResponse })
-            }
-
-
-         })
-   })
-   app.get('/paidRents/:email', (req, res) => {
-      rentCollection.find({ email: req.params.email })
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
-   app.get('/paidRents', (req, res) => {
-      rentCollection.find({})
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
 
    app.post('/addService', (req, res) => {
       // const serviceImg = req.body.base64;
@@ -418,26 +228,34 @@ client.connect(err => {
       // const cause = req.body.cause;
       // const symptoms = req.body.symptoms;
       // const price = req.body.price;
-      const {base64,fileSize,type,title,category,cause,symptoms,price} = req.body
+      const { base64, fileSize, type, title, category, cause, symptoms, price } = req.body
 
       const image = {
          contentType: type,
          size: fileSize,
          img: Buffer.from(base64, 'base64')
       };
-      const serviceInfo = { title: title, category: category, cause: cause, symptoms: symptoms,price: price, ...image }
-       console.log(serviceInfo);
+      const serviceInfo = { title: title, category: category, cause: cause, symptoms: symptoms, price: price, ...image }
+      console.log(serviceInfo);
       serviceCollection.insertOne(serviceInfo)
          .then(result => {
             res.send(result.acknowledged)
          })
    })
+
+
+
    app.get('/allServices', (req, res) => {
+
       serviceCollection.find({})
+
          .toArray((err, documents) => {
             res.send(documents);
          })
    })
+
+
+
    app.delete('/deleteService/:id', (req, res) => {
       serviceCollection.deleteOne({ _id: ObjectId(req.params.id) })
          .then(result => {
@@ -446,7 +264,7 @@ client.connect(err => {
    })
    app.patch('/updateServiceInfo/:id', (req, res) => {
       console.log(req.body)
-      const {title,category,cause,symptoms,price} = req.body
+      const { title, category, cause, symptoms, price } = req.body
       serviceCollection.updateOne({ _id: ObjectId(req.params.id) },
          {
             $set: {
@@ -462,22 +280,7 @@ client.connect(err => {
          });
    })
 
-   app.get('/allMeals', (req, res) => {
-      mealCollection.find({})
-         .toArray((err, documents) => {
-            res.send(documents);
-         })
-   })
 
-   app.patch('/bookedRoom', (req, res) => {
-      roomCollection.updateOne({ _id: ObjectId(req.body.id) },
-         {
-            $set: { vacantStatus: req.body.status }
-         })
-         .then((result) => {
-            res.send(result.modifiedCount > 0)
-         });
-   })
    //room section end
 
 
@@ -489,23 +292,57 @@ client.connect(err => {
       userCollection.find({ email: req.body.email })
          .toArray((err, documents) => {
             const info = { ...documents[0] };
+            // console.log(info)
             if (documents.length) {
                bcrypt.compare(myPassword, info.password, function (err, response) {
                   if (response) {
-                     res.json({info:info,message:"Login successful"});
+                     res.json({ info: info, message: "Login successful", login: true });
                   }
                   else {
-                     res.json({ message: "Email or password is incorrect" });
+                     res.json({ message: "Email or password is incorrect", login: false });
                   }
                });
             } else {
-               res.send({ message: "You are not an user. Please register now." });
+               res.send({ message: "You are not an user. Please register now.", login: false });
             }
          })
 
 
    })
 
+   app.post('/placeOrder', (req, res) => {
+
+      console.log(req.body)
+
+      orderCollection.insertOne(req.body)
+         .then(result => {
+            res.send(result.acknowledged)
+         })
+
+   })
+
+   app.get('/orders', (req, res) => {
+      console.log("hi")
+      orderCollection.find({})
+         .toArray((err, documents) => {
+            res.send(documents);
+         })
+   })
+
+
+   app.patch('/updateStatus', (req, res) => {
+      console.log(req.body)
+      const { status, _id } = req.body
+      orderCollection.updateOne({ _id: ObjectId(_id) },
+         {
+            $set: {
+               status: status
+            }
+         })
+         .then((result) => {
+            res.send(result.modifiedCount > 0)
+         });
+   })
 
 
 });
